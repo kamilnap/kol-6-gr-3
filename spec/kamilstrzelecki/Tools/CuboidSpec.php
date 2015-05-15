9f5f2d7c655903bcd01b6abb91d78e10adf59986
Kamil Strzelecki <kstrzelecki@outlook.com>
2015-05-15 09:38:43 +0200

    cuboid: metody

--- spec/kamilstrzelecki/Tools/CuboidSpec.php
+++ spec/kamilstrzelecki/Tools/CuboidSpec.php
@@ -11,4 +11,20 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamilstrzelecki\Tools\Cuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setH(4)->getH()->shouldReturn(4);
+    }
+    
+    function it_should_calculate_area()
+    {
+        $this->setA(2)->setB(3)->setH(4)->area()->shouldReturn(40);
+    }
+    
+    
+    
+    
 }
--- src/kamilstrzelecki/Tools/Cuboid.php
+++ src/kamilstrzelecki/Tools/Cuboid.php
@@ -4,4 +4,46 @@ namespace kamilstrzelecki\Tools;
 
 class Cuboid
 {
+
+    private $a;
+    private $b;
+    private $h;
+    
+    public function setA($a)
+    {
+        $this->a = $a;
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+        return $this;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function setH($h)
+    {
+        $this->h = $h;
+        return $this;
+    }
+
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function area()
+    {
+        return 2*$this->a*$this->h + 2*$this->b*$this->h;
+    }
 }
