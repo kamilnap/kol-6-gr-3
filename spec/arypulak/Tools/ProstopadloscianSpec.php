d4f9129ca1c097194fcb05895c2712f1c385de25
Arek Rypulak <arekrypulak@o2.pl>
2015-05-29 09:46:36 +0200

    arypulak - cuboid 2

--- spec/arypulak/Tools/ProstopadloscianSpec.php
+++ spec/arypulak/Tools/ProstopadloscianSpec.php
@@ -11,4 +11,16 @@ class ProstopadloscianSpec extends ObjectBehavior
     {
         $this->shouldHaveType('arypulak\Tools\Prostopadloscian');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(345)->getH()->shouldReturn(345);
+    }
+    
+    function it_should_calculate_prostopadloscian()
+    {
+        $this->setA(2)->setB(3)->setH(4)->prostopadloscian()->shouldReturn(40);
+    }
 }
