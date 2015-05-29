b83d06f0f442c68e09fe5bbd84ae0e6ae583a8b6
Bartlomiej Paziewski <bartus.paziewski@gmail.com>
2015-05-29 09:29:44 +0200

    bartuuus/Cuboid: setters and getters

--- spec/bartuuus/Tools/cuboidSpec.php
+++ spec/bartuuus/Tools/cuboidSpec.php
@@ -11,4 +11,16 @@ class cuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('bartuuus\Tools\cuboid');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(222)->getA()->shouldReturn(222);
+        $this->setB(333)->getB()->shouldReturn(333);
+        $this->setH(444)->getH()->shouldReturn(444);
+    }
+
+    function it_should_calculate_area()
+    {
+        $this->setA(2)->setB(3)->setH(4)->area()->shouldReturn(40);
+    }
 }
