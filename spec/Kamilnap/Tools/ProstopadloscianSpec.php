445a659e207396f5b82f8585534c1c23a989d960
Kamil Napiorkowski <kamilnapiorkowski@yahoo.com>
2015-06-12 08:17:04 +0200

    Kamilnap/prostopadloscian: getter

--- spec/Kamilnap/Tools/ProstopadloscianSpec.php
+++ spec/Kamilnap/Tools/ProstopadloscianSpec.php
@@ -11,4 +11,11 @@ class ProstopadloscianSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Kamilnap\Tools\Prostopadloscian');
     }
+    
+     function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(321)->getH()->shouldReturn(321);
+    }
 }
--- src/Kamilnap/Tools/Prostopadloscian.php
+++ src/Kamilnap/Tools/Prostopadloscian.php
@@ -4,4 +4,49 @@ namespace Kamilnap\Tools;
 
 class Prostopadloscian
 {
+
+    private $A;
+    private $B;
+    private $H;
+
+    public function setA($A)
+    {
+        $this->A = $A;
+
+        return $this;
+    }
+
+    public function setB($B)
+    {
+        $this->B = $B;
+
+        return $this;
+    }
+    
+     public function setH($H)
+    {
+        $this->H = $H;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->A;
+    }
+
+    public function getB()
+    {
+        return $this->B;
+    }
+    
+      public function getH()
+    {
+        return $this->H;
+    }
+    
+    function it_should_calculate_wynik()
+{
+    $this->setA(2)->setB(3)->setH(4)->wynik()->shouldReturn(40);
+}
 }
